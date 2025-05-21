3e9fb905e88667e9362d0dccf8b99abbb1b9efe1
SoeRatch <surajkoho@gmail.com>
2025-05-21 13:47:34 +0530

    Docker-based Airflow setup. The official Airflow Docker image expects DAGs to live in ./airflow/dags/

--- airflow/dags/log_etl_dag.py
+++ airflow/dags/log_etl_dag.py
@@ -7,7 +7,7 @@ import sys
 import os
 
 # Let Airflow find our modules
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
 from extract.generator import write_logs
 
