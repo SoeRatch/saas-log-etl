4eba527909f18bfdac554bed8f3e92cfdb9c8556
SoeRatch <surajkoho@gmail.com>
2025-05-23 11:47:54 +0530

    Error handling done in transformer code

--- etl/transform/session_transformer.py
+++ etl/transform/session_transformer.py
@@ -37,25 +37,46 @@ def transform_logs(input_dir, output_dir, execution_date: str):
     input_file = os.path.join(input_dir, f"log_{execution_date}.jsonl")
     output_file = os.path.join(output_dir, f"processed_{execution_date}.jsonl")
 
-    with open(input_file, "r") as infile:
-        raw_logs = [json.loads(line) for line in infile]
+    # Read raw logs
+    try:
+        with open(input_file, "r") as infile:
+            raw_logs = [json.loads(line) for line in infile]
+        logging.info(f"Read {len(raw_logs)} logs from {input_file}")
+    except FileNotFoundError:
+        logging.error(f"Input file not found: {input_file}")
+        return
+    except json.JSONDecodeError as e:
+        logging.error(f"Error decoding JSON in file {input_file}: {e}")
+        return
+    except Exception as e:
+        logging.error(f"Unexpected error while reading {input_file}: {e}")
+        return
+    
 
+    # Transform logs
     transformed = []
-
     for log in raw_logs:
-        transformed.append({
-            "timestamp": log.get("timestamp"),
-            "level": map_event_to_level(log.get("event")),
-            "message": generate_message(log),
-            "user_id": log.get("user_id"),
-            "session_id": log.get("session_id"),
-        })
-    
-    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+        try:
+            transformed.append({
+                "timestamp": log.get("timestamp"),
+                "level": map_event_to_level(log.get("event")),
+                "message": generate_message(log),
+                "user_id": log.get("user_id"),
+                "session_id": log.get("session_id"),
+            })
+        except Exception as e:
+            logging.warning(f"Skipping transform of log due to error: {e}")
+            continue
 
-    with open(output_file, "w") as outfile:
-        for t in transformed:
-            outfile.write(json.dumps(t) + "\n")
-    
-    logging.info(f"Transformed logs written to {output_file}")
+    # Write transformed logs
+    try:
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+        with open(output_file, "w") as outfile:
+            for t in transformed:
+                outfile.write(json.dumps(t) + "\n")
+        
+        logging.info(f"Transformed logs written to {output_file}")
+    except Exception as e:
+        logging.error(f"Failed to write transformed logs to {output_file}: {e}")
     
