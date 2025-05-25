ca3f09e624b90fc4ee3232533a44a36c04e7c65e
SoeRatch <surajkoho@gmail.com>
2025-05-25 16:19:01 +0530

    fixing github action failure

--- common/db_config.py
+++ common/db_config.py
@@ -1,10 +1,16 @@
 import os
 
+def get_env_variable(var_name):
+    value = os.getenv(var_name)
+    if value is None:
+        raise EnvironmentError(f"Required environment variable '{var_name}' not set.")
+    return value
+
 def get_db_config():
     return {
-        "dbname": os.environ["DB_NAME"],
-        "user": os.environ["DB_USER"],
-        "password": os.environ["DB_PASSWORD"],
-        "host": os.environ["DB_HOST"],
-        "port": os.environ["DB_PORT"],
+        "dbname": get_env_variable("DB_NAME"),
+        "user": get_env_variable("DB_USER"),
+        "password": get_env_variable("DB_PASSWORD"),
+        "host": get_env_variable("DB_HOST"),
+        "port": get_env_variable("DB_PORT"),
     }
